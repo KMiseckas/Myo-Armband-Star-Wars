0b5cb7f8d8d2eb03fffba2f1e6253e04a0868f65
Klaudijus Miseckas <klaudijusl@yahoo.com>
2015-10-26 23:17:14 +0000

    Lightning

---
+++ Myo_Powers_5.2/Myo_Powers/Assets/Scripts/Powers/Lightning/Moving Lightning/BoltLineUpdate.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoltLineUpdate : MonoBehaviour
+{
+
+    private Vector3 startNodePos;
+    private Vector3 endNodePos;
+    private Vector3 defaultScale;
+
+    private float length;
+
+    void Start()
+    {
+        defaultScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        transform.LookAt (endNodePos);
+
+        length = Vector3.Distance (startNodePos, endNodePos);
+
+        defaultScale.z = length / 2;
+        transform.localScale = defaultScale;
+    }
+
+    public void SetBoltLine(Vector3 startNode, Vector3 endNode)
+    {
+        startNodePos = startNode;
+        endNodePos = endNode;
+    }
+
+}
---
+++ Myo_Powers_5.2/Myo_Powers/Assets/Scripts/Powers/Lightning/Moving Lightning/DynamicLightning.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DynamicLightning : MonoBehaviour
+{
+
+    [Header ("Lightning Mesh")]
+    public GameObject lightningBoltMesh;
+    public Transform parent;
+
+    [Header ("Lightning Target")]
+    public Transform target;
+
+    [Header ("Lightning Bolt Settings")]
+    public int numberOfSplits = 3;
+    public float maxSplitOffsetZ = 0.2f;
+    public float maxSplitOffsetXY = 0.6f;
+
+    [Header ("Safe Guard For While Loop")]
+    public int maxWhileLoops = 1000;
+
+    private int maxLines;
+
+    private List<BoltLine> newLines = new List<BoltLine> ();
+
+    void Start()
+    {
+        maxLines = (int) Mathf.Pow (2f, numberOfSplits);
+        //Debug.Log (maxLines);
+
+        newLines.Add (new BoltLine (transform.position, target.position));
+
+        int loops = 0;
+
+        while(newLines.Count < maxLines)
+        {
+            //Safe guard, incase something goes wrong and while loop would starts looping endlessly
+            loops++;
+            if(loops > maxWhileLoops)
+            {
+                Debug.LogWarning ("Infinite While Loop During Lightning Bolt Creation !!!!");
+                break;
+            }
+
+            List<BoltLine> tempNewLines = new List<BoltLine> ();
+
+            foreach (BoltLine line in newLines)
+            {
+                //Get middle point node of the current line start and end nodes
+                Vector3 midNode = (line.GetStartNode () + line.GetEndNode ()) / 2;
+
+                //Move the middle point node by a random offset value
+                midNode.x += Random.Range (-maxSplitOffsetXY, maxSplitOffsetXY);
+                midNode.y += Random.Range (-maxSplitOffsetXY, maxSplitOffsetXY);
+                midNode.z += Random.Range (-maxSplitOffsetZ, maxSplitOffsetZ);
+
+                //Create 2 new boltlines out of this single split and add to list of newLines
+                tempNewLines.Add (new BoltLine (line.GetStartNode (), midNode));
+                tempNewLines.Add (new BoltLine (midNode, line.GetEndNode ()));
+            }
+
+            newLines.Clear ();
+            newLines = tempNewLines;
+
+            //Debug.Log ("Looping bolt generation");
+        }
+
+        foreach(BoltLine line in newLines)
+        {
+            GameObject newBoltLine = Instantiate (lightningBoltMesh, line.GetStartNode (), Quaternion.identity) as GameObject;
+            newBoltLine.transform.parent = parent;
+
+            newBoltLine.GetComponent<BoltLineUpdate> ().SetBoltLine (line.GetStartNode (), line.GetEndNode ());
+        }
+
+        //Debug.Log (newLines.Count + "   " + maxLines);
+
+    }
+}
